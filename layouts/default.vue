73375ba99b2375fc9d5aef77209166034dc28297
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2020-11-21 19:37:48 +0100

    footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,5 +26,20 @@
 
 <style lang="scss">
   @import "../assets/scss/style";
+  li.crit-item h2 {
+    margin-top: 0;
+  }
+  ul li {
+    margin-bottom: 0;
+    &::before {
+      display: none;
+    }  
+  }
+.footer__websites a, .footer__sites-publics a {
+    color: #3355e3;
+  }  
+  .footer__description p a {
+    text-decoration: underline;
+  }
 </style>
 
