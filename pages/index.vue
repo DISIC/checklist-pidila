537a3c45bb01b99a94096bdf4871572229aab3e0
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2020-11-21 12:38:11 +0100

    header/footer designGouv

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
   <main id="main" class="main" role="main">
-    <div class="container main-container">
-      <div class="col-group">
-        <div class="criteria-boxes col-main">
-          <h1>Liste des critères</h1>
+<div class="cover">
+  <div class="cover__container">
+  <h1>La <span  class="highlight">checklist</span> PiDila</h1>
+  <p class="cover__subtitle">Une liste unique des bonnes pratiques pour les sites publics : <abbr title="Référentiel général d‘amélioration de l‘accessibilité">RGAA</abbr>, Charte internet et Marque de l‘État, Bonnes pratiques Opquast, Éco-conception, <abbr title="Référentiel général d‘interopérabilité">RGI</abbr> et Loi informatique et liberté.</p>
+  <p>Il est toujours intéressant de faire un diagnostic rapide en utilisant <a href="http://pidila.gitlab.io/amoa/evaluation-rapide.html">la checklist rapide</a> ou bien de faire les tests de la <a href="http://pidila.gitlab.io/developpement/shortlist.html">checklist du développeur</a> avant de livrer.</p>
+  <div class="checklist_container">
+        <div class="criteria-boxes">
 
           <alert v-for="alert in alerts" :key="alert.id" :data="alert"/>
 
@@ -38,9 +41,30 @@
           <column-links/>
 
         </aside-column>
-
+  </div>  </div>
+</div>
+  <div class="pidila-footer">
+    <div class="container">
+      <div class="footer-content">
+        <p>Développée conjointement par le <a href="https://pidila.gitlab.io/" title="Pôle intégration de la DILA">PiDILA</a>  et la <abbr title="Direction interministérielle du numérique">DINUM</abbr>, la Checklist Pidila est désormais maintenue par le pôle <a href="https://design.numerique.gouv.fr/">DesignGouv</a> de la DINUM.</p>
+        <p>Le <a href="https://gitlab.com/pidila/checklist-pidila">code source</a> de l'application servant à afficher cette page est distribué sous double licence <a href="https://gitlab.com/pidila/scampi-twig/blob/master/LICENCE-MIT.md">MIT</a> et <a href="http://www.cecill.info/licences/Licence_CeCILL-B_V1-fr.html">CeCILL-B</a>. Les contenus sont sous licences respectives de chacun des <a href="https://pidila.gitlab.io/checklist/referentiels.html">référentiels</a> alimentant la Checklist Pidila.</p>
+      </div>
+      <div class="pidila-logo">
+        <p><a href="https://design.numerique.gouv.fr" target="_blank">
+          <img src="svg/designgouv.svg" width="120" alt="DesignGouv - nouvelle fenêtre"/>
+        </a>
+      </p>
+      <p>
+        <a href="https://pidila.gitlab.io">
+          <svg class="svg-logo" aria-hidden="true" focusable="false">
+            <use xlink:href="svg/illus-sprite.svg#pidila"></use>
+          </svg>
+          <span class="sr-only">Site du Pidila</span>
+        </a>
+      </p>
       </div>
     </div>
+  </div>
   </main>
 </template>
 
@@ -123,5 +147,27 @@
   }
 </script>
 
-<style>
+<style type="scss">
+.cover__container { max-width: 80em; }
+.checklist_container { margin-top: 2em; display: flex; }
+.criteria-boxes { flex: 1 0 54%; margin-right:2em; }
+.col-aside { 
+  width: 40autoem;
+  text-align: left;
+}
+button { border-radius: unset;}
+.crit-item-test {
+  text-align: left;
+}
+.crit-item-title {
+  font-weight: normal;
+}
+.crit-item-header .js-expandmore-button:hover,
+.crit-item-header .js-expandmore-button:focus, 
+.crit-item-header [aria-expanded="true"],
+.crit-item-title {
+    color: inherit;
+}    
+  
+
 </style>
